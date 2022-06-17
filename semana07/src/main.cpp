a5b9c2accc1f813e9f740136d507b6552b76e72c
CaduRomaniello <cadu_romaniello@hotmail.com>
2022-06-17 09:54:57 -0300

    arrumando diretorios

---
+++ semana07/src/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+    int n;
+    int ones, count;
+    while( scanf( "%d", &n ) != EOF ){
+        ones = 1%n;
+        count = 1;
+        while( ones ){
+            ones *= 10;
+            ones++;
+            ones %= n;
+            count++;
+        }
+
+        printf( "%d\n", count );
+    }
+
+    return 0;
+}
